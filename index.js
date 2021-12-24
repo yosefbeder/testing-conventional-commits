d73dd468be1cf43c1c62dcea9f0b131f15360361
yosefbeder <dryosefbeder@gmail.com>
2021-12-24 10:40:12 +0200

    feat: rename mult to mul

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const add = (n1, n2) => n1 + n2;
 
 const sub = (n1, n2) => n1 - n2;
 
-const mult = (n1, n2) => n1 * n2;
+const mul = (n1, n2) => n1 * n2;
 
 const div = (n1, n2) => n1 / n2;
 
-module.exports = { sub, add, mult, div };
+module.exports = { sub, add, mul, div };
