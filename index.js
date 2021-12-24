46dbbfde357fa1fb9320921d41fa50e3731fd414
yosefbeder <dryosefbeder@gmail.com>
2021-12-24 10:56:52 +0200

    feat: add more than two params

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const add = (n1, n2) => n1 + n2;
+const add = (...ns) => ns.reduce((acc, n) => acc + n, 0);
 
-const sub = (n1, n2) => n1 - n2;
+const sub = (...ns) => ns.reduce((acc, n) => acc - n, 0);
 
-const mul = (n1, n2) => n1 * n2;
+const mul = (...ns) => ns.reduce((acc, n) => acc * n, 0);
 
-const div = (n1, n2) => n1 / n2;
+const div = (...ns) => ns.reduce((acc, n) => acc / n, 0);
 
 module.exports = { sub, add, mul, div };
