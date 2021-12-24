01c51f3c5f9c3dd0d9abf9a3d23ae4a794f3268c
Yosef Beder <dryosefbeder@gmail.com>
2021-12-24 10:52:06 +0200

    More utils (#1)
    * feat: add mult and div
    
    * feat: export mult and div
    
    * feat: rename mult to mul

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const add = (n1, n2) => n1 + n2;
 
 const sub = (n1, n2) => n1 - n2;
 
-module.exports = { sub, add };
+const mul = (n1, n2) => n1 * n2;
+
+const div = (n1, n2) => n1 / n2;
+
+module.exports = { sub, add, mul, div };
