31ca79a66bb4e84259c2e7c7006ae1d07e70391e
yosefbeder <dryosefbeder@gmail.com>
2021-12-25 07:02:41 +0200

    fix: fix all of the utils

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const add = (...ns) => ns.reduce((acc, n) => acc + n, 0);
 
-const sub = (...ns) => ns.reduce((acc, n) => acc - n, 0);
+const sub = (...ns) =>
+	ns.reduce((acc, n, i) => (i === 0 ? acc : acc - n), ns[0]);
 
-const mul = (...ns) => ns.reduce((acc, n) => acc * n, 0);
+const mul = (...ns) =>
+	ns.reduce((acc, n, i) => (i === 0 ? acc : acc * n), ns[0]);
 
-const div = (...ns) => ns.reduce((acc, n) => acc / n, 0);
+const div = (...ns) =>
+	ns.reduce((acc, n, i) => (i === 0 ? acc : acc / n), ns[0]);
 
 module.exports = { sub, add, mul, div };
